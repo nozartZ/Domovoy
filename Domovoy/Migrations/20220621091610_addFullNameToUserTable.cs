f16e3f862a432c5c2e327a772efdae9400dfbbfa
Женя Можиловский <nozartgrizly@gmail.com>
2022-08-01 11:48:44 +0300

    Добавьте файлы проекта.

---
+++ Domovoy/Migrations/20220621091610_addFullNameToUserTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Domovoy.Migrations
+{
+    public partial class addFullNameToUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Domovoy/WC.cs
@@ -0,0 +1,13 @@
+﻿namespace Domovoy
+{
+    public static class WC
+    {
+        public const string ImagePath = @"\images\product\";
+        public const string SessionCart = "ShoppingCartSession";
+
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+
+        public const string EmailAdmin = "nozartgrizly@gmail.com";
+    }
+}
