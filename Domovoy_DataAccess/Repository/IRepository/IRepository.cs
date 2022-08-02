f51656a39ff029738eef4d045c6c2c3c3df1054e
Женя Можиловский <nozartgrizly@gmail.com>
2022-08-02 11:20:49 +0300

    Интерфейс для паттерна Репозиторий

---
+++ Domovoy_DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domovoy_DataAccess.Repository.IRepository
+{
+    public interface IRepository <T> where T : class
+    {
+        T Find (int id);
+        IEnumerable<T> GetAll (
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null,
+            bool isTracking = true
+            );
+    }
+}
