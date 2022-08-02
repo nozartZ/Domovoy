25f22773488506b49c118332d205504b5d7bde4f
Женя Можиловский <nozartgrizly@gmail.com>
2022-08-02 12:54:23 +0300

    Создал репозиторий для Category

---
+++ Domovoy_DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domovoy_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domovoy_DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository:IRepository<Category>
+    {
+        void Update(Category obj);
+    }
+}
