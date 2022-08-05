047c117e653cca5de4077af7c4bfcd7be87692a7
Женя Можиловский <nozartgrizly@gmail.com>
2022-08-05 11:23:55 +0300

    Контроллер запросов и вьюмодель для деталей запроса

---
+++ Domovoy/Controllers/InquiryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Domovoy_DataAccess;
+using Domovoy_Models;
+using System.Collections.Generic;
+using Domovoy_Utility;
+using Domovoy_DataAccess.Repository.IRepository;
+
+namespace Domovoy.Controllers
+{
+    
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inqHRepo;
+        private readonly IInquiryDetailRepository _inqDRepo;
+
+        public InquiryController(IInquiryHeaderRepository inqHRepo, IInquiryDetailRepository inqDRepo)
+        {
+            _inqHRepo = inqHRepo;
+            _inqDRepo = inqDRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Domovoy_Models/ViewModels/InquiryVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domovoy_Models.ViewModels
+{
+    public class InquiryVM
+    {
+        public InquiryHeader inquiryHeader { get; set; }
+        public IEnumerable<InquiryDetail> inquiryDetail { get; set; }
+
+    }
+}
