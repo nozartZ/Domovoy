fbcd2bbc76471cf32941d05d496354e6849e10be
Женя Можиловский <nozartgrizly@gmail.com>
2022-08-11 21:42:29 +0300

    Метод обновления корзины покупок и изменил код index корзины для обновления

--- Domovoy/Controllers/CartController.cs
+++ Domovoy/Controllers/CartController.cs
@@ -185,5 +185,19 @@ namespace Domovoy.Controllers
             TempData[WC.Success] = "Товар удален из корзины";
             return RedirectToAction(nameof(Index));
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateCart(IEnumerable <Product> ProdList)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            foreach (Product prod in ProdList)
+            {
+                shoppingCartList.Add(new ShoppingCart { ProductId = prod.Id, SqFt = prod.TempSqFt });
+            }
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
