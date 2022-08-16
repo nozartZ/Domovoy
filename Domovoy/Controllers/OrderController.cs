3e7aa3f4c2b174d8c943a6a8da7730dffae1ca70
Женя Можиловский <nozartgrizly@gmail.com>
2022-08-16 19:06:46 +0300

    Создал вьюмодель для управления списком заказов

---
+++ Domovoy/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using Domovoy_DataAccess.Repository.IRepository;
+using Domovoy_Models.ViewModels;
+using Domovoy_Utility.BrainTree;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Domovoy.Controllers
+{
+    public class OrderController : Controller
+    {
+        
+        private readonly IOrderHeaderRepository _orderHRepo;
+        private readonly IOrderDetailRepository _orderDRepo;
+        private readonly IBrainTreeGate _brain;
+
+        
+        public ProductUserVM ProductUserVM { get; set; }
+
+        public OrderController(IOrderHeaderRepository orderHRepo, IOrderDetailRepository orderDRepo, IBrainTreeGate brain)
+        {
+            _orderHRepo = orderHRepo;
+            _orderDRepo = orderDRepo;
+            _brain = brain;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
